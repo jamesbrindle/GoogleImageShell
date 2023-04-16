b44c4ed1ff90d66d41062abedff5a637071b8757
Myrkur <myrkur@myrkur.dev>
2023-04-16 19:34:16 -0400

    console alloc debugging added

--- GoogleImageShell/Program.cs
+++ GoogleImageShell/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Runtime.InteropServices;
 using System.Windows.Forms;
 
 namespace GoogleImageShell
@@ -8,14 +9,21 @@ namespace GoogleImageShell
         [STAThread]
         public static void Main(string[] args)
         {
+            
+            [DllImport("kernel32.dll", SetLastError = true)]
+            [return: MarshalAs(UnmanagedType.Bool)]
+            static extern bool AllocConsole();
+            
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             if (args.Length >= 1 && args[0].ToLower() == "search")
             {
+                // AllocConsole();
                 Application.Run(new UploadForm(args));
             }
             else
             {
+                // AllocConsole();
                 Application.Run(new ConfigForm());
             }
         }
