53e7833a5d30446fbe5b1852ad6b66a5c95c4d2c
Myrkur <Myrkur@Myrkur.dev>
2023-10-10 14:17:04 -0400

    version bump to V1.4, vanity

--- GoogleImageShell/Properties/AssemblyInfo.cs
+++ GoogleImageShell/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("GoogleImageShell")]
-[assembly: AssemblyCopyright("Copyright © crossbowffs 2016")]
+[assembly: AssemblyCopyright("Copyright © crossbowffs, Myrkur 2023")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -31,6 +31,5 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.3.0.0")]
-[assembly: AssemblyFileVersion("1.3.0.0")]
+[assembly: AssemblyVersion("1.4.0.0")]
+[assembly: AssemblyFileVersion("1.4.0.0")]
